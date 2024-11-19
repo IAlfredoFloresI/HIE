b760c10d1abba58e54a8e3f05c3699f2ff71246a
Eduardo Flores <eduardoflores121298@gmail.com>
2024-11-19 10:20:58 -0600

    .

--- modules/auth/jwtHelper.js
+++ modules/auth/jwtHelper.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (payload) => {
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '5m' }); // Ajusta el tiempo de expiración si es necesario
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }); // Ajusta el tiempo de expiración si es necesario
 };
 
 module.exports = { generateToken };
